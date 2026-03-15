a823e9af8b2f25cfd33eccb4c94c063e9c7bb912
xXJsonDeruloXx <danielhimebauch@gmail.com>
2026-03-15 18:51:35 -0400

    input: add double-tap click and drag

--- Limelight/Input/RelativeTouchHandler.m
+++ Limelight/Input/RelativeTouchHandler.m
@@ -14,6 +14,9 @@ static const int REFERENCE_WIDTH = 1280;
 static const int REFERENCE_HEIGHT = 720;
 static const CGFloat PINCH_DETECTION_THRESHOLD = 10.0f;
 static const CGFloat PINCH_CONTINUATION_THRESHOLD = 2.0f;
+static const NSTimeInterval DOUBLE_TAP_RECOGNITION_INTERVAL = 0.30;
+static const CGFloat DOUBLE_TAP_DISTANCE_THRESHOLD_FACTOR = 0.35f;
+static const useconds_t SYNTHETIC_CLICK_PRESS_DURATION_US = 50 * 1000;
 
 @implementation RelativeTouchHandler {
     CGPoint touchLocation, originalLocation;
@@ -23,7 +26,11 @@ static const CGFloat PINCH_CONTINUATION_THRESHOLD = 2.0f;
     BOOL isDragging;
     BOOL pinchGestureActive;
     NSTimer* dragTimer;
+    dispatch_queue_t mouseButtonQueue;
     NSUInteger peakTouchCount;
+    NSTimeInterval lastSingleTapTimestamp;
+    CGPoint lastSingleTapLocation;
+    BOOL lastSingleTapValid;
     
 #if TARGET_OS_TV
     UIGestureRecognizer* remotePressRecognizer;
@@ -42,6 +49,10 @@ static const CGFloat PINCH_CONTINUATION_THRESHOLD = 2.0f;
     self = [self init];
     self->view = view;
     self->desktopTrackpadMode = desktopTrackpadMode;
+    self->mouseButtonQueue = dispatch_queue_create("com.moonlight.RelativeTouchHandler.mouseButtons", DISPATCH_QUEUE_SERIAL);
+    self->lastSingleTapTimestamp = 0;
+    self->lastSingleTapLocation = CGPointZero;
+    self->lastSingleTapValid = NO;
     
 #if TARGET_OS_TV
     remotePressRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(remoteButtonPressed:)];
@@ -65,8 +76,49 @@ static const CGFloat PINCH_CONTINUATION_THRESHOLD = 2.0f;
 - (void)onDragStart:(NSTimer*)timer {
     if (!touchMoved && !isDragging){
         isDragging = true;
-        LiSendMouseButtonEvent(BUTTON_ACTION_PRESS, BUTTON_LEFT);
+        [self sendMouseButtonAction:BUTTON_ACTION_PRESS button:BUTTON_LEFT];
     }
+}
+
+- (void)sendMouseButtonAction:(int)action button:(int)button {
+    dispatch_async(mouseButtonQueue, ^{
+        LiSendMouseButtonEvent(action, button);
+    });
+}
+
+- (void)sendSyntheticMouseClick:(int)button {
+    dispatch_async(mouseButtonQueue, ^{
+        LiSendMouseButtonEvent(BUTTON_ACTION_PRESS, button);
+        usleep(SYNTHETIC_CLICK_PRESS_DURATION_US);
+        LiSendMouseButtonEvent(BUTTON_ACTION_RELEASE, button);
+    });
+}
+
+- (void)clearLastSingleTapState {
+    lastSingleTapTimestamp = 0;
+    lastSingleTapLocation = CGPointZero;
+    lastSingleTapValid = NO;
+}
+
+- (void)recordLastSingleTapAtLocation:(CGPoint)location timestamp:(NSTimeInterval)timestamp {
+    lastSingleTapTimestamp = timestamp;
+    lastSingleTapLocation = location;
+    lastSingleTapValid = YES;
+}
+
+- (BOOL)isDoubleTapContinuationForTouch:(UITouch*)touch {
+    if (!lastSingleTapValid) {
+        return NO;
+    }
+
+    if (touch.timestamp - lastSingleTapTimestamp > DOUBLE_TAP_RECOGNITION_INTERVAL) {
+        return NO;
+    }
+
+    CGFloat distanceThreshold = MIN(view.bounds.size.width, view.bounds.size.height) * DOUBLE_TAP_DISTANCE_THRESHOLD_FACTOR;
+    CGPoint currentLocation = [touch locationInView:view];
+    return hypotf(currentLocation.x - lastSingleTapLocation.x,
+                  currentLocation.y - lastSingleTapLocation.y) <= distanceThreshold;
 }
 
 - (BOOL)sendRelativeMouseMoveToLocation:(CGPoint)currentLocation {
@@ -95,18 +147,31 @@ static const CGFloat PINCH_CONTINUATION_THRESHOLD = 2.0f;
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     touchMoved = false;
-    peakTouchCount = [[event allTouches] count];
-    if ([[event allTouches] count] > 1) {
+    NSUInteger touchCount = [[event allTouches] count];
+    peakTouchCount = touchCount;
+    if (touchCount > 1) {
         [dragTimer invalidate];
         dragTimer = nil;
+        [self clearLastSingleTapState];
     }
-    if ([[event allTouches] count] == 1) {
+    if (touchCount == 1) {
         UITouch *touch = [[event allTouches] anyObject];
+        BOOL doubleTapContinuation = !isDragging && [self isDoubleTapContinuationForTouch:touch];
         originalLocation = touchLocation = [touch locationInView:view];
         initialTwoFingerDistance = 0.0f;
         lastTwoFingerDistance = 0.0f;
         pinchGestureActive = NO;
-        if (!isDragging) {
+
+        [dragTimer invalidate];
+        dragTimer = nil;
+
+        if (doubleTapContinuation) {
+            isDragging = YES;
+            [self sendMouseButtonAction:BUTTON_ACTION_PRESS button:BUTTON_LEFT];
+            [self clearLastSingleTapState];
+        }
+        else if (!isDragging) {
+            [self clearLastSingleTapState];
             dragTimer = [NSTimer scheduledTimerWithTimeInterval:0.650
                                                      target:self
                                                    selector:@selector(onDragStart:)
@@ -114,7 +179,7 @@ static const CGFloat PINCH_CONTINUATION_THRESHOLD = 2.0f;
                                                     repeats:NO];
         }
     }
-    else if ([[event allTouches] count] == 2) {
+    else if (touchCount == 2) {
         CGPoint firstLocation = [[[[event allTouches] allObjects] objectAtIndex:0] locationInView:view];
         CGPoint secondLocation = [[[[event allTouches] allObjects] objectAtIndex:1] locationInView:view];
         
@@ -184,39 +249,31 @@ static const CGFloat PINCH_CONTINUATION_THRESHOLD = 2.0f;
     [dragTimer invalidate];
     dragTimer = nil;
     pinchGestureActive = NO;
+    UITouch *endedTouch = [touches anyObject];
+    CGPoint endedLocation = [endedTouch locationInView:view];
+    NSTimeInterval endedTimestamp = endedTouch.timestamp;
     if ([[event allTouches] count] - [touches count] < 2) {
         initialTwoFingerDistance = 0.0f;
         lastTwoFingerDistance = 0.0f;
     }
     if (isDragging) {
         isDragging = false;
-        LiSendMouseButtonEvent(BUTTON_ACTION_RELEASE, BUTTON_LEFT);
+        [self sendMouseButtonAction:BUTTON_ACTION_RELEASE button:BUTTON_LEFT];
+        [self clearLastSingleTapState];
     } else if (!touchMoved) {
         if (peakTouchCount == 2) {
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-                Log(LOG_D, @"Sending right mouse button press");
-                
-                LiSendMouseButtonEvent(BUTTON_ACTION_PRESS, BUTTON_RIGHT);
-                
-                // Wait 100 ms to simulate a real button press
-                usleep(100 * 1000);
-                
-                LiSendMouseButtonEvent(BUTTON_ACTION_RELEASE, BUTTON_RIGHT);
-            });
+            Log(LOG_D, @"Sending right mouse button press");
+            [self sendSyntheticMouseClick:BUTTON_RIGHT];
+            [self clearLastSingleTapState];
         } else if (peakTouchCount == 1) {
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-                if (!self->isDragging){
-                    Log(LOG_D, @"Sending left mouse button press");
-                    
-                    LiSendMouseButtonEvent(BUTTON_ACTION_PRESS, BUTTON_LEFT);
-                    
-                    // Wait 100 ms to simulate a real button press
-                    usleep(100 * 1000);
-                }
-                self->isDragging = false;
-                LiSendMouseButtonEvent(BUTTON_ACTION_RELEASE, BUTTON_LEFT);
-            });
+            Log(LOG_D, @"Sending left mouse button press");
+            [self sendSyntheticMouseClick:BUTTON_LEFT];
+            [self recordLastSingleTapAtLocation:endedLocation timestamp:endedTimestamp];
+        } else {
+            [self clearLastSingleTapState];
         }
+    } else {
+        [self clearLastSingleTapState];
     }
     
     // We we're moving from 2+ touches to 1. Synchronize the current position
@@ -231,6 +288,7 @@ static const CGFloat PINCH_CONTINUATION_THRESHOLD = 2.0f;
         // Mark this touch as moved so we don't send a left mouse click if the user
         // right clicks without moving their other finger.
         touchMoved = true;
+        [self clearLastSingleTapState];
     }
 }
 
@@ -240,9 +298,10 @@ static const CGFloat PINCH_CONTINUATION_THRESHOLD = 2.0f;
     pinchGestureActive = NO;
     initialTwoFingerDistance = 0.0f;
     lastTwoFingerDistance = 0.0f;
+    [self clearLastSingleTapState];
     if (isDragging) {
         isDragging = false;
-        LiSendMouseButtonEvent(BUTTON_ACTION_RELEASE, BUTTON_LEFT);
+        [self sendMouseButtonAction:BUTTON_ACTION_RELEASE button:BUTTON_LEFT];
     }
     peakTouchCount = 0;
 }
